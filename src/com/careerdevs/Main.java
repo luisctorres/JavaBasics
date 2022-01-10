26e55170471271f745b56dd82f0755d8b262013e
Luis Torres <scratch2c@gmail.com>
2022-01-10 18:19:16 -0500

    Initial commit

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,11 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String firstName = "Luis";
+    String lastName = "Torres";
+    String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
